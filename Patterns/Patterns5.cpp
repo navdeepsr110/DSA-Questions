da321d0baebe3d82545cad87ce0fd6b21a73e0c0
Navdeep <rathorenavdeep110@gmail.com>
2022-11-30 18:56:24 +0530

    Pyramid Pattern

--- Patterns/Patterns5.cpp
+++ Patterns/Patterns5.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 
 int main(){
-  cout<<"*"<<endl;
+  int n;
+  cin>>n;
+  for(int i=1;i<=n;i++){
+    for(int j=1;j<=(n-i);j++){
+      cout<<" ";
+    }
+    for(int k=0;k!=2*i-1;k++){
+      cout<<"*";
+    }
+    cout<<endl;
+  }
+  
 }
